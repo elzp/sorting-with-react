4c866c26b422d8cc24402a57137ceecdbf4dbefd
elzp <86740659+elzp@users.noreply.github.com>
2022-10-07 20:15:41 +0200

    Add sorting function

--- Hello.js
+++ Hello.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from './Row';
 export default function () {
-  let data = [
+  let previousdata = [
     { subject: 'Fundamenty języka JavaScript', name: 'Adam', time: 10129 },
     { subject: 'Przeglądarka bez tajemnic', name: 'Przemek', time: 19393 },
     { subject: 'W świecie frameworków', name: 'Marcin', time: 14002 },
   ];
+
+  function sortByTime(thisdata, type) {
+    let newData = [...thisdata];
+    switch (type) {
+      case 'toBig':
+        newData.sort((a, b) => {
+          return a.time - b.time;
+        });
+        break;
+      case 'toSmall':
+        newData.sort((a, b) => {
+          return b.time - a.time;
+        });
+        break;
+      default:
+        newData.sort((a, b) => {
+          return a.time - b.time;
+        });
+        break;
+    }
+    return newData;
+  }
+
+  const mofifiedData = sortByTime(previousdata);
+
+  const [data, changeData] = useState(mofifiedData);
+  console.log(data);
   return (
     <table class="table-auto bg-white rounded-md overflow-hidden shadow-xl">
       <thead>
@@ -14,19 +41,16 @@ export default function () {
           <th class="px-4 py-2">Autor</th>
           <th class="px-4 py-2">
             <a class="hover:text-orange-600" href="#">
-              <i class="fas fa-caret-down"></i> Czas trwania
+              <i class="fas fa-caret-down"></i>
+              Czas trwania
             </a>
           </th>
         </tr>
       </thead>
       <tbody>
-        {data.map((item)=>{
-
-          return (
-            <Row item={item}/>
-          )
-        })
-        }
+        {data.map((item) => {
+          return <Row item={item} key={item.time} />;
+        })}
         {/* <tr>
           <td class="border px-4 py-2">Fundamenty języka JavaScript</td>
           <td class="border px-4 py-2">Adam</td>
