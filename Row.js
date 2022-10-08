bbeed09e0eb0d7b1a41b3027769ced6b92d9c23e
elzp <86740659+elzp@users.noreply.github.com>
2022-10-08 19:48:56 +0200

    Add formating time in table

--- Row.js
+++ Row.js
@@ -3,12 +3,16 @@ import React from 'react';
 export default function Row(props) {
   const { subject, name, time } = props.item;
 
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time - hours * 3600) / 60);
+  const seconds = time - hours * 3600 - minutes * 60;
+  const formatedTime = `${hours}:${minutes}:${seconds}`;
   return (
     <>
       <tr>
         <td class="border px-4 py-2">{subject}</td>
         <td class="border px-4 py-2">{name}</td>
-        <td class="border px-4 py-2">{time}</td>
+        <td class="border px-4 py-2">{formatedTime}</td>
       </tr>
     </>
   );
